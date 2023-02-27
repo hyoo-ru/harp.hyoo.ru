a1b5a7a2cca93c29fb88cc53723236d2bbcaecd5
jin <jin@hyoo.ru>
2023-02-27 14:56:20 +0300

    Reduce inferent type size

--- scheme/scheme.ts
+++ scheme/scheme.ts
@@ -7,15 +7,15 @@ namespace $ {
 		Value extends $mol_data_value< any, any > = typeof $mol_data_integer,
 	>( sub: Sub, value = $mol_data_integer as Value ) {
 		
-		const inner = $mol_data_optional( $mol_data_record( sub ) )
-		const values = $mol_data_optional( $mol_data_array( $mol_data_array( value ) ) )
+		const inner = $mol_data_variant( $mol_data_record( sub ) )
+		const values = $mol_data_variant( $mol_data_array( $mol_data_array( value ) ) )
 		
 		const config = {
 			... sub,
-			'+': $mol_data_optional( $mol_data_boolean ),
+			'+': $mol_data_variant( $mol_data_boolean ),
 			'=': values,
 			'!=': values,
-			'_num': $mol_data_optional( $mol_data_record({
+			'_num': $mol_data_variant( $mol_data_record({
 				'=': $mol_data_array( $mol_data_array( Int ) )
 			}) ),
 		}
