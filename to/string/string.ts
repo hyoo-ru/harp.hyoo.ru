c9049f1faaf2dd683e8ccdba99fd154584a49cdb
jin <jin@hyoo.ru>
2023-03-04 17:39:09 +0300

    Fixed escaping

--- to/string/string.ts
+++ to/string/string.ts
@@ -15,14 +15,14 @@ namespace $ {
 			
 			let values = ( ( harp['='] || harp['!='] || [] ) as unknown[][] ).map( ([ min , max ]) => {
 
-				if( max === undefined || min === max ) return encodeURIComponent( String( min ) )
+				if( max === undefined || min === max ) return encodeURIComponent( String( min ) ) + '='
 				
 				min = ( min === undefined ) ? '' : encodeURIComponent( String( min ) )
 				max = ( max === undefined ) ? '' : encodeURIComponent( String( max ) )
 				
-				return `${ min }@${ max }`
+				return `${ min }@${ max }=`
 
-			} ).join( ',' )
+			} ).join( '' )
 			
 			let fetch = $hyoo_harp_to_string( harp as $hyoo_harp_query )
 			if( fetch ) fetch = `(${fetch})`
